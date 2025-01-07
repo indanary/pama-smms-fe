c07aebc7c16c0bbeab72a97e3ff13bbec222816d
indanary <indanary@gmail.com>
2025-01-07 17:27:48 +0700

    setup axios and token

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,31 +1,61 @@
-import { defineBoot } from '#q-app/wrappers';
-import axios, { type AxiosInstance } from 'axios';
+import { defineBoot } from '#q-app/wrappers'
+import axios, { type AxiosInstance } from 'axios'
+import { useTokenStore } from 'src/stores/token'
 
 declare module 'vue' {
   interface ComponentCustomProperties {
-    $axios: AxiosInstance;
-    $api: AxiosInstance;
+    $axios: AxiosInstance
+    $api: AxiosInstance
   }
 }
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' });
+const apiBaseUrl = process.env.VUE_APP_API_BASE_URL || ''
+
+const api = axios.create({ baseURL: apiBaseUrl, timeout: 60000 })
+
+const unauthApi = axios.create({ baseURL: apiBaseUrl, timeout: 60000 })
+
+api.interceptors.request.use(
+  async (config) => {
+    const tokenStore = useTokenStore()
+
+    if (tokenStore.getAccessToken()) {
+      if (tokenStore.isTokenExpired()) {
+        // todo: call refresh token
+      }
+
+      // add auth header
+      config.headers['Authorization'] = 'Bearer ' + tokenStore.getAccessToken()
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+api.interceptors.response.use(
+  (res) => {
+    return res
+  },
+  async (err) => {
+    // error status 401 = not authorized
+    // force logout user
+    if (err.response?.status == 401) {
+      const tokenStore = useTokenStore()
+      tokenStore.forceLogoutUser()
+    }
+
+    return Promise.reject(err)
+  },
+)
 
 export default defineBoot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+  app.config.globalProperties.$axios = axios
 
-  app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
+  app.config.globalProperties.$api = api
+  app.config.globalProperties.$unauthApi = unauthApi
+})
 
-  app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
-});
-
-export { api };
+export { api, unauthApi }
---
+++ src/stores/token.ts
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+import { deleteCookie, getCookie } from 'app/utils/cookie'
+
+export const useTokenStore = defineStore('token', {
+  state: () => ({}),
+
+  actions: {
+    getAccessToken(): string | null {
+      return getCookie('accessToken')
+    },
+
+    isTokenExpired(): boolean {
+      try {
+        const accessToken: string = this.getAccessToken() ?? ''
+
+        const parts = accessToken.split('.')
+        if (parts.length !== 3 || !parts[1]) {
+          throw new Error('Invalid token format')
+        }
+
+        const payloadBase64Url = parts[1]
+        const payloadBase64 = payloadBase64Url.replace(/-/g, '+').replace(/_/g, '/')
+        const payloadJson = atob(payloadBase64)
+        const payload = JSON.parse(payloadJson)
+
+        const currentTime = Math.floor(Date.now() / 1000) // Current time in seconds
+
+        if (payload.exp) {
+          return currentTime >= payload.exp
+        }
+
+        // If no expiration claim, consider it not expired by default
+        return false
+      } catch (error) {
+        console.error('Failed to decode token:', error)
+        return true // If there's an error, assume the token is invalid or expired
+      }
+    },
+
+    forceLogoutUser(): void {
+      deleteCookie('accessToken', '/')
+      deleteCookie('refreshToken', '/')
+
+      window.location.reload()
+    },
+  },
+})
